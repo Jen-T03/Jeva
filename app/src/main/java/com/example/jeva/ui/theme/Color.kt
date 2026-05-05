5d757e02136cf7e592ac87590b30489fa1b1a4ca
TU_NOMBRE <TU_CORREO>
2026-05-05 18:54:52 -0500

    Se arreglan colores y tema definiendo colores.

--- app/src/main/java/com/example/jeva/ui/theme/Color.kt
+++ app/src/main/java/com/example/jeva/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.example.jeva.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-
 val BlackPrimary = Color(0xFF0D0D0D)
 val RedPrimary = Color(0xFFC21515)
+val RedSecondary = Color(0xFF8B0000)  // ← rojo más oscuro
 val DarkGray = Color(0xFF1C1C1C)
 val WhiteText = Color(0xFFFFFFFF)
--- app/src/main/java/com/example/jeva/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jeva/ui/theme/Theme.kt
@@ -12,32 +12,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = RedPrimary,
+    secondary = RedSecondary,
+    tertiary = RedPrimary,
+    background = BlackPrimary,
+    surface = BlackPrimary,
+    onPrimary = WhiteText,
+    onSecondary = WhiteText,
+    onBackground = WhiteText,
+    onSurface = WhiteText
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = RedPrimary,
+    secondary = RedSecondary,
+    tertiary = RedPrimary,
+    background = BlackPrimary,
+    surface = BlackPrimary,
+    onPrimary = WhiteText,
+    onSecondary = WhiteText,
+    onBackground = WhiteText,
+    onSurface = WhiteText
 )
 
 @Composable
 fun JevaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -45,7 +46,6 @@ fun JevaTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
